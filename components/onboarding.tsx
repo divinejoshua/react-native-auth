8cc9940e99ef4f03675f8c0d9b24f53f91c4a116
divinejoshua <divineuche399@gmail.com>
2023-08-12 13:03:10 +0100

    Added lottie package

--- components/onboarding.tsx
+++ components/onboarding.tsx
@@ -2,19 +2,63 @@ import { View, Text } from '../components/Themed'
 // @ts-ignore : true 
 import Onboarding from 'react-native-onboarding-swiper';
 import React from 'react'
-import { Image } from 'react-native';
+import Lottie from 'lottie-react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Dimensions, StyleSheet } from 'react-native';
+
+
+const {width, height} = Dimensions.get('window');
+
 
 export default function OnboardingScreen() {
   return (
+    <>
+    <StatusBar hidden={true} />
         <Onboarding
           pages={[
             {
               backgroundColor: '#fff',
-              // image: <Image source={require('./images/circle.png')} />,
+              image: (
+                <View style={styles.lottie}>
+                    <Lottie source={require('../assets/animations/boost.json')} autoPlay loop />
+                </View>
+              ),
+
               title: 'Onboarding',
-              subtitle: 'Done with React Native Onboarding Swiper',
+              subtitle: 'Welcome to Cloudiby, we help you store and manage your files the 21st century way.',
             },
           ]}
+
+          titleStyles={{
+            // color: '#fff',
+          }}
+          subTitleStyles={{
+            textAlign: 'left',
+            paddingHorizontal: 10
+
+          }}
+          imageContainerStyles={{
+          }}
         />
+
+    </>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: 'white'
+  },
+  lottie:{
+      width: width*0.9,
+      height: width
+  },
+  doneButton: {
+      padding: 20,
+      // backgroundColor: 'white',
+      // borderTopLeftRadius: '100%',
+      // borderBottomLeftRadius: '100%'
+  }
+})
