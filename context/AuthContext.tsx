e95c93b83af6fa6c994eb436db853814af21841b
divinejoshua <divineuche399@gmail.com>
2023-08-20 14:49:18 +0100

    Added login function in context

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,8 +1,10 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import axios from "../api/axios";
+import * as SecureStore from 'expo-secure-store';
+
 
 interface AuthProps {
-    authState : { token: string | null, authenticated: boolean | null}
+    authState : { token: string | null}
     onLogin: (username: string, password : string) => Promise<any>
     onLogout: (token : string) => Promise<any>
 }
@@ -13,7 +15,6 @@ const TOKEN_KEY : string = ""  //Token key from expo secure store
 const AuthContext = createContext<AuthProps>({
     authState: {
         token: null,
-        authenticated: null
     },
     onLogin: function (username: string, password: string): Promise<any> {
         throw new Error("Function not implemented.");
@@ -31,6 +32,49 @@ export const useAuth = () => {
 
 // Auth provider 
 export const AuthProvider = ({children} : any) => {
+
+    //Data
+    
+    const [authState, setauthState] = useState<{
+        token : string | null,
+    }>({
+        token : null,
+    })
+
+    // Login function 
+    const login = async (username: string, password: string) => {
+
+        // Attempt 
+        try {
+            // @ts-ignore 
+            let response = await axios.get('/accounts/check/', { username, password });
+
+            console.log (response.data)
+
+            setauthState({
+                token : response.data.access_token
+            })
+
+            // Set the access token as the authorisation data 
+            axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.access_token}`
+
+            //Set the access token to expo secure store 
+            await SecureStore.setItemAsync(TOKEN_KEY, response.data.access_token)
+
+            return response
+        }
+
+        // Exception 
+        catch (error) {
+            console.log (error)
+        }
+
+    }
+
+    // Logout function 
+    // const logout 
+
+
 
     // Context value 
     const contextValue = {}
